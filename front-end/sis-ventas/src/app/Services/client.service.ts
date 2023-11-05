5ee0392ed17642717ce1750b40bc44b74917724c
KevinCS29 <kevin.cumbicus@unl.edu.ec>
2023-11-04 21:29:33 -0500

    Actualizacion service client

--- front-end/sis-ventas/src/app/Services/client.service.ts
+++ front-end/sis-ventas/src/app/Services/client.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { ResponseApi } from '../interfaces/response-api';
+import { Client } from '../interfaces/client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  private url: string = environment.url_api+'/client';
+
+  constructor(private http:HttpClient) {}
+
+  list(): Observable<ResponseApi> {
+    return this.http.get<ResponseApi>(`${this.url}/list/`);
+  }
+
+  create(request: Client): Observable<ResponseApi> {
+    return this.http.post<ResponseApi>(`${this.url}/add/`, request);
+  }
+
+  update(id: string, request: Client): Observable<ResponseApi> {
+    return this.http.put<ResponseApi>(`${this.url}/update/${id}/`, request);
+  }
+
+  delete(id: string): Observable<ResponseApi> {
+    return this.http.delete<ResponseApi>(`${this.url}/delete/${id}/`);
+  }
 }
